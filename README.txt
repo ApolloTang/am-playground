150f0292cf867f18f1f38e45cdaf782acb0842c2
Apollo Tang <tang.apollo@gmail.com>
2015-06-16 18:10:36 -0400

    less and sass are compile into separate output files

---
+++ README.txt
@@ -0,0 +1,11 @@
+to run:
+
+$ npm install
+$ gulp -p<port-number>
+
+enter in browser: localhost:<port-number>
+
+
+
+
+
---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+$ = jQuery = require('./libs/jquery.js');
+require('./libs/bootstrap.js');
+var hello = require('./modules/hello');
+
+hello.sayHello();
+
+$('#ef-creative-builder').modal('show');
+
+$('#creative-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+$('#creative-tab a[data-target="#customize"]').tab('show');
+
+$('#asset-1-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+$('#asset-2-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+$('#asset-3-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+$('#asset-4-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+$('#asset-5-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+
+$('#asset-6-tab a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
